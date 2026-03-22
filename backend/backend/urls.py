91e8f46b71f2e8db4e0f759869e3983020746f3f
Daniel Bautista <danibaufu@gmail.com>
2026-03-21 21:41:50 -0600

    feat(api): integrate Swagger UI using drf-spectacular with custom grouping

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/users/', include('users.urls')),
     path('api/crm/', include('crm.urls')),
     path('api/products/', include('products.urls')),
