f7f8ae9ef2529be05d6cb16d1e780646664ce7ff
Daniel Bautista <danibaufu@gmail.com>
2026-05-21 16:41:58 -0600

    fix excel export non registered url

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from orders.order.apis.views import OrderAPIView, OrderCatalogAPIView, OrderDetailAPIView
+from orders.order.apis.views import OrderAPIView, OrderCatalogAPIView, OrderDetailAPIView, OrderExportExcelAPIView
 from orders.order_history.apis.views import OrderHistoryAPIView
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('statuses/', include('orders.order_status.urls')),
     path('items/', include('orders.order_item.urls')),
     path('notifications/', include('orders.order_notification.urls')),
+    path('export/excel/', OrderExportExcelAPIView.as_view(), name='orders-export-excel'),
     path('', OrderAPIView.as_view(), name='orders-list-create'),
     path('<int:pk>/', OrderDetailAPIView.as_view(), name='orders-detail'),
     path('catalogs/', OrderCatalogAPIView.as_view(), name='orders-catalogs'),
